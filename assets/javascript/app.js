3b566b7e1239dfe77f68fec40d8ad2885f313a39
Ana Chernov <anastasia.chernov@gmail.com>
2019-10-23 14:50:45 -0400

    Added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,13 +11,9 @@ $("#showHide").on("click", function() {
   }
 });
 
-// $(".button").on("click", function () {
-//     var answer = $(this).text();
-//     if (answer === question[counter].correct) {
-//         correct++;
-//     }
-//     counter++;
-// display next question using questions[counter].question
+var correct = 0;
+var incorrect = 0;
+var questionsIndex = 0;
 
 var questions = [
   {
@@ -57,3 +53,100 @@ var questions = [
     image: "assets/images/hawaii.jpeg"
   }
 ];
+
+// HERE IS THE TIMER
+var timer = 10;
+var intervalId;
+
+// Present user with only "start game" div on load
+$(document).ready(function() {
+  $("#quiz-area").hide();
+  $("#timer-area").hide();
+  $("#results").hide();
+});
+
+// User clicks start game to start, "start game" div is hidden
+// (Creating an on-click event that responds when user clicks start)
+$(document).on("click", "#start-button", function() {
+  $("#start-div").hide();
+  $("#quiz-area").show();
+  $("#timer-area").show();
+  $("#results").hide();
+  writeQuestion();
+});
+
+// Counter loops through questions array and displays first question
+function writeQuestion() {
+  $("#question").text(questions[questionsIndex].question);
+  $("#buttonA").text(questions[questionsIndex].answer1);
+  $("#buttonB").text(questions[questionsIndex].answer2);
+  $("#buttonC").text(questions[questionsIndex].answer3);
+  $("#buttonD").text(questions[questionsIndex].answer4);
+}
+
+function wrongAnswer() {
+  $("#start-div").hide();
+  $("#quiz-area").hide();
+  $("#timer-area").hide();
+  $("#results").show();
+  $("#correctText").text(questions[questionsIndex].correct);
+}
+
+// Timer starts at 20 seconds and begins counting down
+
+function run() {
+  intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+  timer--;
+
+  //  Show the nummer in the #show-timer tag.
+  $("#show-timer").html("<h2>" + timer + "</h2>");
+
+  //  Once number hits zero...// If no user input (timer runs out), go to wrong div
+  if (timer === 0) {
+    //  ...run something?
+    wrongAnswer();
+  }
+}
+
+run();
+
+// User selects an answer, computer listens for click
+$(".answer-button").on("click", function() {
+  console.log("answer was selected");
+
+  // This checks to see if selected answer is correct
+  var selectedAnswer = $(this).text();
+  if (selectedAnswer === questions[questionsIndex].correct) {
+    correct++;
+    console.log(correct);
+  }
+});
+
+// Results div is shown (question div is hidden)
+// If selected answer is correct, user sees correct div
+// If selected answer is wrong, user sees wrong div
+
+// Click continue to move on
+$(document).on("click", "#next-question", function() {
+  $("#start-div").hide();
+  $("#quiz-area").show();
+  $("#timer-area").show();
+  $("#results").hide();
+  writeQuestion();
+});
+
+// Time remaining resets to 30 seconds
+
+// When we get to the end of questions, you see final results screen with correct, incorrect, and unanswered tallies
+
+// Per Shelby
+// $(".button").on("click", function () {
+//   var answer = $(this).text();
+//   if (answer === question[counter].correct) {
+//       correct++;
+//   }
+//   counter++;
+// display next question using questions[counter].question

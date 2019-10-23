a14dbedb0212aedd40ebb4c7dd4b9ddd9d13959a
Ana Chernov <anastasia.chernov@gmail.com>
2019-10-22 21:42:42 -0400

    Figured out hide and show feature

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,59 @@
+// var isHidden = false;
 
-$(".button").on("click", function () {
-    var answer = $(this).text();
-    if (answer === question[counter].correct) {
-        correct++;
-    }
-    counter++;
-    // display next question using questions[counter].question
-}
+// Here I figured out how to show and hide objects. When one is showing, the other is not.
+$("#showHide").on("click", function() {
+  if ($("#quiz-area").is(":visible")) {
+    $("#quiz-area").hide();
+    $("#results").show();
+  } else {
+    $("#quiz-area").show();
+    $("#results").hide();
+  }
+});
 
+// $(".button").on("click", function () {
+//     var answer = $(this).text();
+//     if (answer === question[counter].correct) {
+//         correct++;
+//     }
+//     counter++;
+// display next question using questions[counter].question
 
 var questions = [
-    {
-        question: "What is the most popular spice in the world?",
-        answer1: "cumin",
-        answer2: "pepper",
-        answer3: "oregano",
-        answer4: "turmeric",
-        correct: "pepper",
-        image: "assets/images/pepper.jpg"
-    },
-    {
-        question: "Monterey Jack is a type of what?",
-        answer1: "cheese",
-        answer2: "pizza",
-        answer3: "olive",
-        answer4: "fish",
-        correct: "cheese",
-        image: "assets/images/cheese.jpg"
-    },
-    {
-        question: "What is the national dish of England?",
-        answer1: "pot roast",
-        answer2: "fish & chips",
-        answer3: "chicken tikka masala",
-        answer4: "welsh rarebit",
-        correct: "chicken tikka masala",
-        image: "assets/images/tikka.jpg"
-    },
-    {
-        question: "What is the only US State which grows coffee beans?",
-        answer1: "hawaii",
-        answer2: "florida",
-        answer3: "california",
-        answer4: "texas",
-        correct: "hawaii",
-        image: "assets/images/hawaii.jpeg"
-    },
+  {
+    question: "What is the most popular spice in the world?",
+    answer1: "cumin",
+    answer2: "pepper",
+    answer3: "oregano",
+    answer4: "turmeric",
+    correct: "pepper",
+    image: "assets/images/pepper.jpg"
+  },
+  {
+    question: "Monterey Jack is a type of what?",
+    answer1: "cheese",
+    answer2: "pizza",
+    answer3: "olive",
+    answer4: "fish",
+    correct: "cheese",
+    image: "assets/images/cheese.jpg"
+  },
+  {
+    question: "What is the national dish of England?",
+    answer1: "pot roast",
+    answer2: "fish & chips",
+    answer3: "chicken tikka masala",
+    answer4: "welsh rarebit",
+    correct: "chicken tikka masala",
+    image: "assets/images/tikka.jpg"
+  },
+  {
+    question: "What is the only US State which grows coffee beans?",
+    answer1: "hawaii",
+    answer2: "florida",
+    answer3: "california",
+    answer4: "texas",
+    correct: "hawaii",
+    image: "assets/images/hawaii.jpeg"
+  }
 ];
